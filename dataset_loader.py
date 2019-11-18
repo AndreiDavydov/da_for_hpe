f735aea4ab5d4b3989cfa8ac1e45b988852dfd7d
Davydov Andrey <davydov@iccvlabsrv26.iccluster.epfl.ch>
2019-11-18 16:45:11 +0100

    first commit: H36M, MPII datasets prepared, DAMN architecture also

---
+++ dataset_loader.py
@@ -0,0 +1,280 @@
+from matplotlib.pyplot import imread
+import pickle
+import json
+import random
+
+import torch as th
+import numpy as np
+from torch.utils.data import Dataset
+
+
+'''
+This module holds datasets for HPE task. 
+'''
+
+PATH_H36M = '/cvlabdata2/cvlab/Human36m/OpenPose/'
+PATH_MPII = '/cvlabsrc1/cvlab/datasets_victor/MPII_HumanPose/'
+
+class H36M(Dataset):
+    '''
+    Provides H3.6M Dataset.
+    '''
+    def __init__(self, PATH_H36M=PATH_H36M, num_images=1000, mode='train'):
+
+        if not ((mode == 'train') or (mode == 'val')):
+            raise Exception(
+                'Incorrect mode type! Only "train" or "val" are available.')
+        self.PATH_H36M = PATH_H36M
+        self.mode = mode
+        self.num_images = num_images
+
+        pkl_path = self.PATH_H36M+mode+'_data.pkl'
+        with open(pkl_path, 'rb') as f:
+            self.pkl = pickle.load(f)
+
+        scens_delete = []
+        for scenario in self.pkl:
+            if self.pkl[scenario] == {}:
+                scens_delete.append(scenario)
+        for scen in scens_delete:
+            del self.pkl[scen]
+        self.scenarios = list(self.pkl.keys())
+
+        self.subjects = (1,5,6,7,8) if mode == 'train' else (9,11)
+        self.num_cameras = 8
+
+    def __len__(self):
+        return self.num_images
+
+    def get_item(self, idx):
+        ''' The problem is that it is hard to prepare dataset as a list 
+        of datapaths. Hence, we propose the technique of getting i's sample
+        based on the total number of images only.
+        '''
+        num_per_scen = int(self.num_images//
+            len(self.scenarios))
+        num_per_subj = int(self.num_images//
+            (len(self.scenarios)*len(self.subjects)))
+        num_per_cam = int(self.num_images//
+            (len(self.scenarios)*len(self.subjects)*self.num_cameras)) 
+
+        scen = idx // num_per_scen
+        scen = len(self.scenarios) - 1 if scen >= len(self.scenarios) else scen
+        idx -= scen * num_per_scen
+        subj = idx // num_per_subj
+        subj = len(self.subjects) - 1 if subj >= len(self.subjects) else subj
+        idx -= subj * num_per_subj
+        cam = idx // num_per_cam
+        cam = self.num_cameras - 1 if cam >= self.num_cameras else cam
+        idx -= cam * num_per_cam
+
+        return scen, subj, cam, idx
+
+    # def transforms(self, image, depth):
+
+    #     assert self.crop_size < min(depth.shape), \
+    #     'Inconsistent crop size, it is bigger than image dimensions!'
+    #     y0 = np.random.randint(0,image.shape[0] - self.crop_size)
+    #     x0 = np.random.randint(0,image.shape[1] - self.crop_size)
+
+    #     image = image[y0:y0+self.crop_size, x0:x0+self.crop_size]
+    #     depth = depth[y0:y0+self.crop_size, x0:x0+self.crop_size]
+    #     return image, depth
+
+    def __getitem__(self, idx):
+        scen, subj, cam, idx = self.get_item(idx)
+
+        scen = self.scenarios[scen]
+        subj = self.subjects[subj]
+        cam = list(self.pkl[scen][subj].keys())[cam]
+        name = list(self.pkl[scen][subj][cam].keys())[idx]
+
+        datapoint = self.pkl[scen][subj][cam][name]
+
+        img_path = PATH_H36M+'S'+str(subj)+'/Images/'+cam+'_000000'+name+'.jpg'
+        image = th.tensor(imread(img_path)/255).permute(2,0,1)
+
+        # heatmap = imread(PATH_H36M+datapoint['heatmap_path']).reshape(368,15,368)
+        joints = th.tensor(datapoint['annotations_2d'])
+        image, joints = prepare_input(image, joints)
+
+        return image, joints
+
+        # _3d_ = one_data['annotations_3d'].reshape(-1,3)
+        # x_, y_, z_ = _3d_[:,0], _3d_[:,1], _3d_[:,2] 
+        # fig,ax = plt.subplots(1,1,figsize=(10,10))
+        # ax.set_xlim((-1000,1000))
+        # ax.set_ylim((1000,-1000))
+
+        # ax.plot(x_,y_, 'ro')
+        # for i in range(len(_3d_)):
+        # #     print(_3d_[i])
+        #     plt.annotate(i, (_3d_[i,0], _3d_[i,1]))
+
+class MPII(Dataset):
+    '''
+    Provides MPII Dataset.
+    '''
+    def __init__(self, PATH_MPII=PATH_MPII, num_images=1000, mode='train', seed=0):
+
+        self.seed = seed
+
+        if not ((mode == 'train') or (mode == 'val')):
+            raise Exception(
+                'Incorrect mode type! Only "train" or "val" are available.')
+
+        self.PATH_MPII = PATH_MPII
+        self.mode = mode
+        self.num_images = num_images
+
+        mean = th.load(self.PATH_MPII+'Images/mean.pth')
+        self.mean = mean['mean']
+        self.std = mean['std']
+
+        annotations = json.load(open(PATH_MPII+'Annotations/mpii_annotations.json', 'rb'))
+        self.annotations, self.ids = self._initialize_annotations(annotations)
+
+    def __len__(self):
+        return self.num_images
+
+    def _initialize_annotations(self, annotations):
+        ''' Firstly, we follow "easy" strategy, processing MPII dataset.
+        It likes to contain several people on same image, but keeping them
+        in different "annotation" dictionaries. 
+        We'd like to delete ALL non-unique annotations.
+        Then suffle these indices and cut the necessary part of them.
+        '''
+
+        # All this part till shuffle can be done with the preprocess!!!
+        # TODO!!!
+        unique_paths, ids_to_delete = [], []
+        for idx,annot in enumerate(annotations):
+            img_path = annot['img_paths']
+            if img_path in unique_paths:
+                ids_to_delete.append(idx)
+            else:
+                unique_paths.append(img_path)
+
+        for idx in ids_to_delete[::-1]:
+            del annotations[idx]
+
+        # Now let's remain only "mode" data.
+        mode_ids = []
+        mode_key = 1 if self.mode == 'val' else 0
+        for idx, annot in enumerate(annotations):
+            if annot['isValidation'] == mode_key:
+                mode_ids.append(idx)
+
+        random.seed(self.seed)
+        random.shuffle(mode_ids)
+        mode_ids = mode_ids[:self.num_images]
+        return annotations, mode_ids
+
+
+    def __getitem__(self, idx):
+        annot = self.annotations[self.ids[idx]]
+        img_path = self.PATH_MPII + 'Images/images/' + annot['img_paths']
+        image = th.tensor((imread(img_path)/255)).permute(2,0,1)
+
+        joint_self = annot['joint_self']
+        joints = th.tensor([point[:2] for point in joint_self]) # 16 x 2
+        joints = map_joints(joints) # rename all joints acc. to the H36M protocol
+
+        joints_valid = joints.sum(dim=1) != 0 # 0,0 - non-visible points
+        joints = joints[joints_valid]
+
+        image, joints = prepare_input(image, joints)
+
+        return image, joints, joints_valid
+
+
+def do_pad(img, joints):
+    H,W = (int(img.shape[-2]), int(img.shape[-1]))
+    
+    if H > W:
+        total_pad = H - W
+        pad_l, pad_r = total_pad//2, total_pad - total_pad//2
+        pad = (pad_l,pad_r, 0,0)
+        img = th.nn.functional.pad(img, pad)
+        joints[:,0] += pad_l
+    elif W > H:
+        total_pad = W - H
+        pad_b, pad_t = total_pad//2, total_pad - total_pad//2
+        pad = (0,0, pad_b,pad_t)
+        img = th.nn.functional.pad(img, pad)  
+        joints[:,1] += pad_b
+    # otherwise, dimensions already coincide
+    
+    return img, joints
+
+def prepare_input(img, joints):
+    ''' Prepares data for ResNet50 architecture, 
+    img_size = C x 224 x 224.
+
+
+    Joints must change correspondingly.
+
+    img - tensor, C x H x W
+    joints - tensor, N x 2 # for the case of 2d HPE
+    '''
+    out_size = (224,224) # as ResNet50 requires
+
+
+    joint_l, joint_r = int(joints[:,0].min()), int(joints[:,0].max())
+    joint_b, joint_t = int(joints[:,1].min()), int(joints[:,1].max())
+
+    H_box, W_box = int(joint_t-joint_b), int(joint_r-joint_l)
+    center_box = (joint_l+W_box/2, joint_b+H_box/2)
+
+    # img = C x H x W
+    # computing box coordinates in the global system (img coordinates)
+    box_l = int(np.maximum(center_box[0]-W_box, 0))
+    box_r = int(np.minimum(center_box[0]+W_box, img.shape[-1]))
+    box_t = int(np.minimum(center_box[1]+H_box, img.shape[-2]))
+    box_b = int(np.maximum(center_box[1]-H_box, 0))
+
+    # adjusting joints to the box
+    joints[:,0] -= box_l
+    joints[:,1] -= box_b
+
+    img = img[..., box_b:box_t, box_l:box_r]
+
+    # now let's pad image to make it square
+    img, joints = do_pad(img, joints)
+    
+    # now scale img and joints to the "out_size"
+    scale = img.shape[-1] / out_size[-1] # assuming scale is same for x and y
+    img = th.nn.functional.interpolate(img.unsqueeze(0), out_size, 
+            mode='bilinear',align_corners=True)[0]
+    img.clamp_(0,1)
+    joints /= scale
+
+    return img, joints
+
+def map_joints(joints_mpii):
+    ''' Maps the MPII format of joints encoding to the 
+    format of the H36M.
+    0 -> 10; 1 -> 9; 2 -> 8; 3 -> 11; 4 -> 12; 5 -> 13;
+    8 -> 1; 9 -> 0; 10 -> 4; 11 -> 3; 12 -> 2; 13 -> 5; 14 -> 6; 15 -> 7;
+    (6 + 7) -> 14
+    '''
+    joints_h36m = joints_mpii.clone()[:-1]
+    joints_h36m[[10,9,8,11,12,13]] = joints_mpii[[0,1,2,3,4,5]]
+    joints_h36m[[1,0,4,3,2,5,6,7]] = joints_mpii[[8,9,10,11,12,13,14,15]]
+    joints_h36m[14] = (joints_mpii[6] + joints_mpii[7])/2
+    return joints_h36m
+
+# def get_rect(l,r,t,b):
+#     return [[l,l,r,r,l], 
+#             [b,t,t,b,b]]
+
+
+
+
+
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,247 @@
+import torch as th
+import torch.nn as nn
+from resnet.resnet import resnet50
+
+PATH_RESNET50_PRETRAINED = './resnet/resnet50.pth'
+
+class DamNet(nn.Module):
+    ''' Produces DAMNet (based on the DAMN paper)
+    with the ResNet-50 as a backbone.
+    As written in the original paper, this model
+    "encodes sample X from domain d into a feature 
+    vector Z = f(X, d)".
+
+    # named_children of ResNet50: 
+    ## layer0: "conv1", "bn1", "relu", "maxpool" 
+    ## "layer1", "layer2", "layer3", "layer4"
+    ## "avgpool", "fc"
+    '''
+    def __init__(self, num_domains=2, pretrained=True, seed=0, debug=False):
+        super(DamNet, self).__init__()
+
+        th.manual_seed(seed)
+        self.num_domains = num_domains
+        self.num_flows = num_domains 
+        # can vary from unit to unit, here it is fixed
+
+        ########### for checking correct ResNet50 classification quality.
+        self.debug = debug
+        ###########
+
+        flows = []
+        for _ in range(self.num_flows): 
+            flow = resnet50(pretrained=pretrained)
+            if pretrained:
+                flow.load_state_dict(th.load(PATH_RESNET50_PRETRAINED)) 
+            flow = self._compose_flow(flow)
+            flows.append(flow)                
+                                                                        
+        self.units_names = flow.keys()                           
+        self.units = self._compose_units(flows)   
+
+        self.gates_ws, self.plasts_ws = self._init_gates()
+
+    def _init_gates(self):
+        gates_ws, plasts_ws = nn.ParameterDict(), nn.ParameterDict() 
+        # HOW TO DO IT CORRECTLY, ParamDict of ParamDicts?
+        for domain_ind in range(self.num_domains):
+            key = 'domain_'+str(domain_ind)
+            for unit_name in self.units_names:
+                # Not clear how to init plasticities correctly. 
+                # So far all gates are equal at the first epoch.
+                key_unit = key+'_'+unit_name
+                gates_ws[key_unit] = nn.Parameter(
+                    th.randn((self.num_flows, 1,1,1,1)), requires_grad=True)
+                plasts_ws[key_unit] = nn.Parameter(-th.log(
+                    th.tensor(1/(1/self.num_flows) - 1))/ \
+                        (gates_ws[key_unit].detach()))
+
+        return gates_ws, plasts_ws
+
+    def _compose_flow(self, flow, arch_name='resnet'):
+        ''' Takes the original backbone network and transforms it in 
+        the "flow" structure, where after each unit the gate transformation 
+        is applied.
+        ''' 
+        if arch_name == 'resnet':
+            d = dict(flow.named_children())
+
+            # combines first four transformations in one layer
+            # ("conv1" in DAMNet notation for ResNet-50)
+            d['layer0'] =[]
+            for key in ['conv1', 'bn1', 'relu', 'maxpool']: 
+                d['layer0'].append(d[key])
+                del d[key]
+            d['layer0'] = nn.Sequential(*d['layer0'])
+
+            # Composes correctly ordered dictionary of flow layers
+            ordered_keys = ['layer0', 'layer1', 'layer2', 'layer3', 'layer4', \
+                            'avgpool', 'fc']
+            flow = th.nn.ModuleDict()
+            for key in ordered_keys:
+                flow[key] = d[key]
+
+            if not self.debug:
+                del flow['avgpool']
+                del flow['fc']
+
+            return flow
+
+    def _compose_units(self, flows):
+        ''' Takes flows prepared by _compose_units and 
+        rearranges theirs modules to be run unit by unit.
+        '''
+        units = nn.ModuleDict()
+        for key in flows[0].keys(): 
+            unit = th.nn.ModuleList()
+            for flow in flows:
+                unit.append(flow[key])
+            units[key] = unit
+
+        return units
+
+    def _aggregation(self, outs, gates_ws, plasts_ws):
+        ''' Performs gate transformation, denoted in the paper 
+        as the "aggregation operator". for the given 
+        outputs of some computational unit and corresponding 
+        gates weights (denoted as "gates_ws" in the signature).
+        It is also suppossed that the size of "outs" is
+        ( N_flows x B x C x H x W ).
+
+        params:
+        outs      - stack of outputs of some computational unit
+        gates_ws  - "g" coefs, trainable parameters
+        plasts_ws - "pi" coefs, plasticity parameters. 
+                    Increasing over time accordint to the schedule.
+        '''
+        if outs.ndim != 5:
+            # case of resnet "fc" layer, dim is decreased
+            return outs.mean(dim=0)
+
+        # correct 5-ndim layer case
+        gates = th.sigmoid(gates_ws*plasts_ws)
+        return (outs * gates).sum(dim=0)
+
+    def forward(self, x, domain_ind):
+        '''
+        Computes output of the DAMNet. 
+        x - input vector, batch of images.
+        domain_ind - some index (or key) of the domain, 
+                     to be able to choose what gates to learn.
+        '''
+        key = 'domain_'+str(domain_ind) \
+                if isinstance(domain_ind, int) else domain_ind
+
+        assert (int(key[-1]) <= self.num_domains-1), \
+            'domain index is incorrect, must be from {}, but {} was given.'.\
+            format(list(range(self.num_domains)), int(key[-1]))
+
+        for unit_name in self.units_names:
+            unit = self.units[unit_name]
+
+            if unit_name == 'fc':
+                x = th.flatten(x,1)
+
+            outs = []
+            # print(unit_name)
+            for flow_ind in range(len(unit)):
+                out = unit[flow_ind](x)
+                outs.append(out)
+            outs = th.stack(outs)
+
+            x = self._aggregation(outs, self.gates_ws[key+'_'+unit_name], 
+                            self.plasts_ws[key+'_'+unit_name])
+            # print(x.shape)
+        return x
+
+
+class GradientReversalFunction(th.autograd.Function):
+    """
+    Gradient Reversal Layer from:
+    Unsupervised Domain Adaptation by Backpropagation (Ganin & Lempitsky, 2015)
+    Forward pass is the identity function. In the backward pass,
+    the upstream gradients are multiplied by -lambda (i.e. gradient is reversed)
+    
+    The implementation is taken from 
+    https://github.com/jvanvugt/pytorch-domain-adaptation/blob/master/utils.py
+    Thanks to author.
+    """
+
+    @staticmethod
+    def forward(ctx, x, lambda_):
+        ctx.lambda_ = lambda_
+        return x.clone()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        lambda_ = ctx.lambda_
+        lambda_ = grad_output.new_tensor(lambda_)
+        dx = -lambda_ * grad_output
+        return dx, None
+
+class GradientReversal(th.nn.Module):
+    def __init__(self, lambda_=1):
+        super(GradientReversal, self).__init__()
+        self.lambda_ = lambda_
+
+    def forward(self, x):
+        return GradientReversalFunction.apply(x, self.lambda_)
+
+
+class DomainClassifier(nn.Module):
+    ''' Produces Domain Classifier network (F_d) that
+    predicts a D-dimensional vector of domain probabilities d_hat
+    given the feature vector Z from the DAMNet.
+    d_hat = F_d(R(Z)), where R - the gradient reversal pseudo-function
+    from the DANN paper.
+    '''
+    def __init__(self, num_features=2048, inner_features=1024,
+                    num_domains=2, lambd=1,seed=0):
+        super(DomainClassifier, self).__init__()
+        th.manual_seed(0)
+        self.num_domains = num_domains
+
+        self.gradreverse = GradientReversal(lambd)
+
+        fc1 = nn.Linear(num_features, inner_features) 
+        fc2 = nn.Linear(inner_features, inner_features)
+        fc3 = nn.Linear(inner_features, self.num_domains) 
+        self.layers = nn.Sequential(*[
+                fc1, nn.ReLU(inplace=True),
+                fc2, nn.ReLU(inplace=True),
+                fc3])
+
+    def forward(self, z):
+        z = self.gradreverse(z)
+        z = self.layers(z)
+        z = th.sigmoid(z)
+        return z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
